720d6946c2fe8a135cc5488ac73221036e03f0d1
Kamoliddin <161958565+KamolBobo@users.noreply.github.com>
2025-01-13 11:29:01 +0500

    Update bot_send.py

--- services/bot_send.py
+++ services/bot_send.py
@@ -5,9 +5,9 @@ headers = {
     'Accept': 'application/json',
 }
 def send_msg(**kwargs):
-    token = "6754543518:AAEKV335o7NiQ-i-Ze_IWXypIBWCwJ0jTLw"  # bot token
+    token = "TOKEN"  # bot token
 
-    user_id = "5445752576"  # user id
+    user_id = "Telegram_id"  # user id
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + user_id + "&text=" + f"{datetime.now().strftime(f'<b>%d/%m/%y  %H : %M : %S  {kwargs} </b> ')}&parse_mode=HTML"
     response = requests.get(url_req)
     print(response.json())
